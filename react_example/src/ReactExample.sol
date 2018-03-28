eec73c95f68c3ed3db2d09c69814ba8a6287acd3
Tenzin Wangdhen <sinzin91@gmail.com>
2018-03-28 06:57:48 -0700

    add state to smart contract

--- react_example/src/ReactExample.sol
+++ react_example/src/ReactExample.sol
@@ -4,14 +4,24 @@ contract ReactExample {
   address private owner;
   string public you_awesome;
   string private secret;
+  string private state;
   function ReactExample () public {
     owner = msg.sender;
     you_awesome = "You're awesome";
     secret = "secret data";
+    state = "Initial state";
   }
 
   function getSecret () public view returns (string) {
     return secret;
+  }
+
+  function getState() public view returns (string) {
+    return state;
+  }
+
+  function setState(string newState) public payable {
+    state = newState;
   }
 
   function kill () public {
